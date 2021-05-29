d6719791b531ef9f70815c299cea2b307fea38d5
gihan9310 <rathnayakaegb.soft@gmail.com>
2021-05-29 19:57:01 +0530

    second commit with back log reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import backlog_reducer from "./backlog_reducer";
 import error_reducer from "./error_reducer";
 import project_reducer from "./project_reducer";
 
 export default combineReducers({
   errors: error_reducer,
-  project:project_reducer
+  project:project_reducer,
+  backLog:backlog_reducer
 });
